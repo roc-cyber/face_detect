70592b1c374cda50627af3e096d42043627e10ad
Souvik das <62799193+roc-cyber@users.noreply.github.com>
2020-09-09 10:42:08 +0530

    Add files via upload

---
+++ faceopen.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  7 10:25:47 2020
+
+@author: Souvik
+"""
+
+import cv2
+face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cap=cv2.VideoCapture(0)
+while True:
+    _,img=cap.read()
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces=face_cascade.detectMultiScale(gray,1.1,4)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),0.5)
+    cv2.imshow('img',img)
+    k=cv2.waitKey(30)&0xff
+    if k==27:
+        break
+cap.release()
